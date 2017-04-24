5931bc1bccb6952ab2a7b08c835af91018b01ff1
suryasadhana <suryasadhana74@gmail.com>
2017-04-24 15:59:07 +0530

    Create prgm3.c

---
+++ prgm3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the letter");
+scanf("%c",&c);
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+{
+printf("vowels");
+}
+else
+{
+printf("not a vowel");
+}
+getch();
+}
